66dd2fef85a2301b466278feecf0b58a3a95a394
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-04-12 11:17:57 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,14 +1,15 @@
-
 #Vpc
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "jenkins_vpc"
+  name = "eks_cluster_vpc"
   cidr = var.vpc_cidr
 
-  azs                  = data.aws_availability_zones.azs.names
-  public_subnets       = var.public_subnets
-  private_subnets      = var.private_subnets
+  azs             = data.aws_availability_zones.azs.names
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -26,4 +27,37 @@ module "vpc" {
     "kubernetes.io/role/private_elb"       = 1
 
   }
+}
+
+#EKS
+
+module "eks" {
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instance_types
+    }
+  }
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
