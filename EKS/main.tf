588e0e080c06a28378a1f48d2fa9e2eee35ca5c6
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-04-12 10:37:14 +0530

    added files

---
+++ EKS/main.tf
@@ -0,0 +1,29 @@
+
+#Vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins_vpc"
+  cidr = var.vpc_cidr
+
+  azs                  = data.aws_availability_zones.azs.names
+  public_subnets       = var.public_subnets
+  private_subnets      = var.private_subnets
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = 1
+
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/private_elb"       = 1
+
+  }
+}
